8846990f6124bd643c90f648e2ca414379cf56d9
Y Nguyen <cabietdibonhe@gmail.com>
2025-12-22 12:27:08 +0700

    Fix DuckDuckGo home page logic

--- projects/duckduckgo/pages/home_page.py
+++ projects/duckduckgo/pages/home_page.py
@@ -1,12 +1,48 @@
-from selenium.webdriver.common.by import By
-from framework.core.base_page import BasePage
+#from selenium.webdriver.common.by import By
+#from framework.core.base_page import BasePage
 
-class DuckHomePage(BasePage):
-    SEARCH_INPUT = (By.ID, "searchbox_input")
+#class DuckHomePage(BasePage):
+    #SEARCH_INPUT = (By.ID, "searchbox_input")
+
+    #def open_home(self, base_url: str):
+        #self.open(base_url)
+
+    #def search(self, text: str):
+        #self.type(self.SEARCH_INPUT, text + "\n")
+
+
+
+
+# ví dụ DuckHomePage
+#from framework.core.base_page import BasePage
+#from selenium.webdriver.common.by import By
+
+#class DuckHomePage(BasePage):
+    #SEARCH_INPUT = (By.ID, "searchbox_input")
+
+    #def __init__(self, driver):
+        #super().__init__(driver)   # đảm bảo BasePage init logger + wait
+
+    #def open_home(self, base_url: str):
+        #self.open(base_url)
+
+    #def search(self, text: str):
+        #self.type(self.SEARCH_INPUT, text + "\n")
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class DuckHomePage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
 
     def open_home(self, base_url: str):
-        self.open(base_url)
-
-    def search(self, text: str):
-        self.type(self.SEARCH_INPUT, text + "\n")
+        self.driver.get(base_url)
+    def search(self, text):
+        search_box = self.wait.until(
+            EC.visibility_of_element_located(("id", "searchbox_input"))
+        )
+        search_box.send_keys(text)
+        search_box.submit()
 
