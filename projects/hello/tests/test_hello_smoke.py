bf36b0e9d5b0ce5fdb72a08c7341e7fb7259bbc2
Y Nguyen <cabietdibonhe@gmail.com>
2025-12-31 10:53:34 +0700

    Change the file to send notification to slack

--- projects/hello/tests/test_hello_smoke.py
+++ projects/hello/tests/test_hello_smoke.py
@@ -9,3 +9,11 @@ def test_hello_title_and_text(driver, base_url):
     assert "Hello World" in driver.title
     assert page.get_h1_text() == "Hello World"
 
+
+def test_fail_for_screenshot(driver, base_url):
+    driver.get(base_url)
+
+    # Cố tình fail để kích hoạt screenshot
+    assert "THIS_WILL_FAIL" in driver.title
+
+
