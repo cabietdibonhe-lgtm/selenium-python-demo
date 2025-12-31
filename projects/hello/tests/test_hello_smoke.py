71e7451395892ebc50f08f17f9ae9b38c797cf7b
Y Nguyen <cabietdibonhe@gmail.com>
2025-12-31 11:03:49 +0700

    Change the file to send notification to slack

--- projects/hello/tests/test_hello_smoke.py
+++ projects/hello/tests/test_hello_smoke.py
@@ -17,3 +17,16 @@ def test_fail_for_screenshot(driver, base_url):
     assert "THIS_WILL_FAIL" in driver.title
 
 
+import pytest
+import time
+
+@pytest.mark.smoke
+def test_force_fail_to_get_screenshot(driver, base_url):
+    driver.get(base_url)
+    time.sleep(1)  # đợi trang render xong
+
+    # cố tình fail
+    assert "THIS_SHOULD_FAIL" in driver.title
+
+
+
